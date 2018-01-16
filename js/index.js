0a460c8e621a5fdf4717cfe87487c23dd1a4d629
kei <137094562@qq.com>
2018-01-16 18:27:58 +0800

    品牌形象 80%

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $(function(){
 	initMainFocus()
+	initBrandImageFocus()
 });
 
 function initMainFocus(){
@@ -8,4 +9,26 @@ function initMainFocus(){
 	    paginationClickable: true,
 	    spaceBetween: 30,
 	});
+}
+
+function initBrandImageFocus(){
+	var swiper = new Swiper('#brandImage-Focus', {
+		effect: 'coverflow',
+		initialSlide :3,
+		grabCursor: true,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		coverflowEffect: {
+			rotate: 0,
+		    stretch: 200,
+		    depth: 100,
+		    modifier: 1,
+		    slideShadows : true,
+		},
+		on: {
+		        slideChangeTransitionEnd: function(){
+		          // alert(this.activeIndex);//切换结束时，告诉我现在是第几个slide
+		    },
+		}
+    });
 }
