786f7404f7ef5d13cb7737b1605505735eaeee26
kei <137094562@qq.com>
2018-01-17 16:23:18 +0800

    首页 95% 差 QQ 微信 电话

--- js/index.js
+++ js/index.js
@@ -1,16 +1,62 @@
 $(function(){
-	initMainFocus()
-	initBrandImageFocus()
+	initMainFocus();// 首页  主 聚焦图 初始化
+	initBrandImageFocus();// 品牌形象 3D聚焦图 初始化
+	initPhoneAnimate(); // 顶部 电话号码 动画
+	initTabCheck();// 加载tab切换效果
 });
 
+//回到顶部
+function backToTop(){
+	document.documentElement.scrollTop = document.body.scrollTop =0;
+}
+
+// 加载tab切换效果
+function initTabCheck(){
+	initTabCheck_Core("#news-consultation-block");
+	initTabCheck_Core("#Broadcasts-Columns");
+	initTabCheck_Core("#Brand-image");
+	initTabCheck_Core("#product-center");
+}
+
+// tab切换 核心方法
+function initTabCheck_Core(parentId){
+	var title = $( parentId + " .tab-title-sons");
+	var card = $( parentId + " .tab-content-card");
+	title.on("mouseenter",function(){
+		var index= $(this).index();
+		title.removeClass("tab-title-sons-select");
+		$(this).addClass("tab-title-sons-select");
+		if( index > card.length - 1 ){
+			return false;
+		}		
+		card.removeClass("tab-card-show");
+		$(card[index]).addClass("tab-card-show");
+	})
+}
+
+// 顶部 电话号码 动画
+function initPhoneAnimate(i){
+	var node = $(".phone-animate span");
+	var length = node.length;
+	var index = ( ( i < length ) ? i : 0 ) || 0;
+	node.removeClass("font-size-18");
+	$(node[index]).addClass("font-size-18");
+	setTimeout(function(){
+		initPhoneAnimate( index+=1 );
+	},500)
+}
+
+// 首页  主 聚焦图 初始化
 function initMainFocus(){
-	var swiper = new Swiper('.swiper-container', {
+	var swiper = new Swiper('#main-focus', {
 	    pagination: '.swiper-pagination',
 	    paginationClickable: true,
 	    spaceBetween: 30,
+	    autoplay:true
 	});
 }
 
+// 品牌形象 3D聚焦图 初始化
 function initBrandImageFocus(){
 	var swiper = new Swiper('#brandImage-Focus', {
 		effect: 'coverflow',
